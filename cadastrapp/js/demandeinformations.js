41d850eea9a2e7298347a34f25649cdfd112a2c9
laurentcornic <laurent.cornic@gfi.fr>
2015-05-21 17:54:26 +0200

    ajout combobox, redimensionnement des champs, preparation tableau parcelle, section, champ

--- cadastrapp/js/demandeinformations.js
+++ cadastrapp/js/demandeinformations.js
@@ -12,6 +12,45 @@ Ext.namespace("GEOR")
      *  Create ...TODO
      */
     onClickDemand = function(){
+    // formulaire Parcelles
+    var formParcelles = Ext.form;
+
+    // the column model has information about grid columns
+    // dataIndex maps the column to the specific data field in
+    // the data store (created below)
+    var colformParcelles = new Ext.grid.ColumnModel({
+        // specify any defaults for each column
+       defaults: {
+            sortable: true, // columns are not sortable by default ,
+			fieldLabel: 'Parcelle(s)'		
+        },
+        columns: [{
+            id: 'Section',
+            header: 'Section',
+            dataIndex: 'section',
+//			xtype: 'combo',
+            width: 220
+            // use shorthand alias defined above
+ /*           editor: new fm.TextField({
+                allowBlank: false
+            })*/
+        }, {
+            header: 'Parcelle',
+            dataIndex: 'parcelle',
+            width: 130,
+
+  /*           editor: new fm.ComboBox({
+                typeAhead: true,
+                triggerAction: 'all',
+                // transform the data already specified in html
+                transform: 'light',
+//                lazyRender: true,
+                listClass: 'x-combo-list-small' 
+            })*/
+		
+       
+      }] 
+    });
 
 		var demandWindow;
 			demandWindow = new Ext.Window({
@@ -19,45 +58,71 @@ Ext.namespace("GEOR")
 			frame: true,
 			bodyPadding: 10,
 			autoScroll:true,
-			width: 355,
+			width: 450,
 			minimizable: true,
             closable: true,
             resizable: true,
 			draggable : true,
 			fieldDefaults: {
 				labelAlign: 'right',
-				labelWidth: 115,
+
 				msgTarget: 'side'
 		},
 	    items: [{
 			xtype: 'fieldset',
 			title: 'Informations sur le demandeur',
+			labelWidth: 120,
 			defaultType: 'textfield',
 			items: [
-				{ fieldLabel: 'Nom', name: 'nom'},
-				{ fieldLabel: 'Prénom', name: 'prenom'},
-				{ fieldLabel: 'Ville, Commune', name: 'commune'},
-				{ fieldLabel: 'N° de voirie et rue', name: 'numero'},
-				{ fieldLabel: 'Rue', name: 'rue'},
-				{ fieldLabel: 'Lieu-Dit', name: 'lieudit'},
-				{ fieldLabel: 'CNI', name: 'cni'}
+				{ fieldLabel: 'Nom', name: 'nom', width: 280},
+				{ fieldLabel: 'Prénom', name: 'prenom', width: 280},
+				{ fieldLabel: 'Ville, Commune', name: 'commune', width: 280},
+//				{ labelWidth: 60, fieldLabel: '', name: ''},
+				                    {
+                        xtype: 'compositefield',
+                        fieldLabel: 'N° de voirie et rue',
+                        items: [
+                           {
+                               name : 'numero',
+                               xtype: 'numberfield',
+                               width: 40
+                           },
+                           {
+                               xtype: 'combo',
+                               name: 'complement',
+							   width: 40
+                           },
+                           {
+                               xtype: 'textfield',
+							   name: 'rue',
+                               width: 190
+                           }
+                        ]
+                    },
+				{ fieldLabel: 'Lieu-Dit', name: 'lieudit', width: 280},
+				{ fieldLabel: 'CNI', name: 'cni', width: 280}
 				]
 			},
 			{
 			xtype: 'fieldset',
 			title: 'Biens à consulter',
 			defaultType: 'textfield',
+			labelWidth: 120,
 			items: [				
-				{ fieldLabel: 'Ville, Commune', name: 'commune'},
-				{ fieldLabel: 'Parcelle(s)', name: 'parcelle'},
-				{ fieldLabel: 'Adresse(s) cadastrale(s)'}
+				{ fieldLabel: 'Ville, Commune', name: 'commune', width: 280},
+				{ fieldLabel: 'Parcelle(s)', name: 'parcelle'
+//					{xtype: 'grid', name:'colformParcelles'}
+				},
+				{ fieldLabel: 'Adresse(s) cadastrale(s)', name: 'adresse'}
 				]
 			}],
-        buttons: [{
-            text: 'Annuler la demande'
-			},{
-            text: 'Imprimer la demande'
-			}]
+			buttons: [{
+				labelAlign: 'left',				
+				text: 'Annuler la demande'
+				},{
+				labelAlign: 'right',
+				text: 'Imprimer la demande'
+				}]
         });
 		
 		demandWindow.show();
