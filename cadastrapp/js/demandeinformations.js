0df214df7a8e0ef31d802a74841e3543df15a14f
laurentcornic <laurent.cornic@gfi.fr>
2015-05-22 16:25:18 +0200

    mise en forme champ parcelle (demande information foncieres)

--- cadastrapp/js/demandeinformations.js
+++ cadastrapp/js/demandeinformations.js
@@ -13,44 +13,117 @@ Ext.namespace("GEOR")
      */
     onClickDemand = function(){
     // formulaire Parcelles
-    var formParcelles = Ext.form;
-
-    // the column model has information about grid columns
-    // dataIndex maps the column to the specific data field in
-    // the data store (created below)
-    var colformParcelles = new Ext.grid.ColumnModel({
-        // specify any defaults for each column
-       defaults: {
-            sortable: true, // columns are not sortable by default ,
-			fieldLabel: 'Parcelle(s)'		
-        },
-        columns: [{
-            id: 'Section',
-            header: 'Section',
-            dataIndex: 'section',
-//			xtype: 'combo',
-            width: 220
-            // use shorthand alias defined above
- /*           editor: new fm.TextField({
-                allowBlank: false
-            })*/
-        }, {
-            header: 'Parcelle',
-            dataIndex: 'parcelle',
-            width: 130,
-
-  /*           editor: new fm.ComboBox({
-                typeAhead: true,
-                triggerAction: 'all',
-                // transform the data already specified in html
-                transform: 'light',
-//                lazyRender: true,
-                listClass: 'x-combo-list-small' 
-            })*/
+		//liste des compléments de numéro de rue : BIS, TER (à compléter ?)
+		var bisStore = new Ext.data.JsonStore({
+			fields : ['name', 'value'],
+			data   : [
+				{name : '--',   value: '--'},
+				{name : 'bis',  value: 'bis'},
+				{name : 'ter', value: 'ter'}
+			]
+		});	
 		
-       
-      }] 
-    });
+		//liste des sections : TODO : charger dynamiquement selon la ville choisie
+		var sectionStore = new Ext.data.JsonStore({
+			fields : ['name', 'value'],
+			data   : [
+				{name : 'sect1',   value: 'sect1'},
+				{name : 'sect2',  value: 'sect2'},
+				{name : 'sect3', value: 'sect3'}
+			]
+		});
+		
+		//liste des parcelles : TODO : charger dynamiquement selon la ville choisie et la section choisie
+		var parcelleStore = new Ext.data.JsonStore({
+			fields : ['name', 'value'],
+			data   : [
+				{name : 'parc1',   value: 'parc1'},
+				{name : 'parc2',  value: 'parc2'},
+				{name : 'parc3', value: 'parc3'}
+			]
+		});
+		
+		//liste des villes : TODO : récupérer la liste entière
+		var cityStore = new Ext.data.JsonStore({
+			fields : ['name', 'value'],
+			data   : [
+				{name : 'Caen',   value: 'caen'},
+				{name : 'Rennes',  value: 'rennes'},
+				{name : 'Lannion', value: 'lannion'}
+			]
+		});
+	
+		//listes des section / parcelles saisies : "références"
+		//initialement vide
+		//ajoute automatique une ligne vide quand la dernière ligne est complètement remplie
+		//actuellement, on ne peut pas supprimer une ligne
+		var ds = new Ext.data.JsonStore({
+			fields : ['section', 'parcelle'],
+			data   : [{section : '',   parcelle: ''}],
+			listeners: {
+				update(store, record, operation) {
+					var lastIndex = this.getCount()-1;
+					var lastData = this.getAt(this.getCount()-1).data;
+					
+					if (lastData.section!='' && lastData.parcelle!='') {
+						var p = new this.recordType({section:'', parcelle:''}); // create new record
+						parcelleGrid.stopEditing();
+						this.add(p); // insert a new record into the store (also see add)
+						parcelleGrid.startEditing(lastIndex+1, 0);	//
+					}
+				}
+			}
+		});
+
+		//modele la la grille des "références"
+		var colModel = new Ext.grid.ColumnModel([
+			{
+				id:'section',
+				dataIndex: 'section',
+				header: "Section",
+				width: 100,
+				sortable: false,
+				editor: new Ext.form.ComboBox({
+					mode: 'local',
+					value: '',
+					forceSelection: true,
+					editable:       true,
+					displayField:   'name',
+					valueField:     'value',
+					store: sectionStore
+				})
+			},
+			{
+				id: "parcelle",
+				dataIndex: 'parcelle',
+				header: "Parcelle",
+				width: 100,
+				sortable: false,
+				editor: new Ext.form.ComboBox({
+					mode: 'local',
+					value: '',
+					forceSelection: true,
+					editable:       true,
+					displayField:   'name',
+					valueField:     'value',
+					store: parcelleStore
+				})
+			}
+		]);			
+		
+		//grille "références"
+		var parcelleGrid = new Ext.grid.EditorGridPanel({
+			fieldLabel: 'Parcelle(s)',
+			name: 'parcelles',							
+			xtype: 'editorgrid',
+			clicksToEdit: 1,
+			ds: ds,
+			cm: colModel,
+			autoExpandColumn: 'parcelle',
+			height: 100,
+			width: 300,
+			border: true
+		});
 
 		var demandWindow;
 			demandWindow = new Ext.Window({
@@ -83,8 +156,8 @@ Ext.namespace("GEOR")
                         fieldLabel: 'N° de voirie et rue',
                         items: [
                            {
-                               name : 'numero',
                                xtype: 'numberfield',
+							   name : 'numero',
                                width: 40
                            },
                            {
@@ -110,10 +183,98 @@ Ext.namespace("GEOR")
 			labelWidth: 120,
 			items: [				
 				{ fieldLabel: 'Ville, Commune', name: 'commune', width: 280},
-				{ fieldLabel: 'Parcelle(s)', name: 'parcelle'
-//					{xtype: 'grid', name:'colformParcelles'}
+				{ xtype:'tabpanel',
+				activeTab: 0,
+				items:[{
+				
+					//ONGLET 1
+					title:'R&eacute;f&eacute;rence',
+					layout:'form',
+					defaultType: 'displayfield',
+					id: 'firstForm',
+					fileUpload: true,
+					
+					items: [
+					parcelleGrid,	//grille "références"
+					{
+						value: 'ou',
+						fieldClass: 'displayfieldCenter'
+					}]
+											
+				},{
+				
+					//ONGLET 2
+					title:'Adresse castrale',
+					layout:'form',
+					defaultType: 'displayfield',
+					id: 'secondForm',
+
+					items: [{
+						xtype: 'combo',
+						fieldLabel: 'Ville, Commune',
+						name: 'city',
+						width: 300,
+						mode: 'local',
+						value: '',
+						forceSelection: true,
+						editable:       true,
+						displayField:   'name',
+						valueField:     'value',
+						store: cityStore
+					},
+					{
+						value: 'ex. Rennes, Cesson-S&eacute;vign&eacute;',
+						fieldClass: 'displayfieldGray'
+					},
+					{
+						xtype: 'compositefield',
+						fieldLabel: 'N de voirie et rue',
+						defaults: {flex: 1},
+						items: [
+							{
+								name : 'streetNumber',
+								xtype: 'textfield',
+								width: 50,
+							},
+							{
+								name : 'streetBis',
+								xtype: 'combo',
+								width: 50,
+								mode: 'local',
+								value: '--',
+								triggerAction:  'all',
+								forceSelection: true,
+								editable:       false,
+								displayField:   'name',
+								valueField:     'value',
+								store: bisStore
+							},
+							{
+								name : 'streetName',
+								xtype: 'textfield',
+								width: 190
+							}
+						]
+					},
+					{
+						value: 'ex. 4 avenue Henri Fr&eacute;ville',
+						fieldClass: 'displayfieldGray'
+					},
+					{
+						xtype: 'textfield',
+						fieldLabel: 'Lieu-dit',
+						name: 'town',
+						width: 300
+					},
+					{
+						value: 'ex. Mont-Romain, La morinaie',
+						fieldClass: 'displayfieldGray'
+					}
+					]
+				}]
 				},
-				{ fieldLabel: 'Adresse(s) cadastrale(s)', name: 'adresse'}
+
+				{ fieldLabel: 'Adresse(s) cadastrale(s)', name: 'adresse', width: 280}
 				]
 			}],
 			buttons: [{
