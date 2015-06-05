a8bf6ff365aa371ec09dc6b2f13d0afc6bb1c88a
mehdisriti <mehdi.sriti@gfi.fr>
2015-06-05 10:55:51 +0200

    reformating detailUniteCadastrale

--- cadastrapp/js/detailUniteCadastrale.js
+++ cadastrapp/js/detailUniteCadastrale.js
@@ -7,39 +7,39 @@
 Ext.namespace("GEOR")
 
 
-  	 /** public: method[onClickDisplayFIUC]
+       /** public: method[onClickDisplayFIUC]
      *  :param layer: 
      *  Create ...TODO
      */
     onClickDisplayFIUC = function(){
-		//déclaration des variables relatives aux données cadastrales
-		var ccodep, ccodir, ccocom, gcopre, ccosec, dnupla, dnupro, dnomlp, dprnlp, expnee, dnomcp, dprncp, adressehabitation , jdatnss , dldnss, ccodro_lib, dnvoiri, dindic, natvoiriv_lib, dvoilib, dcntpa, supf, gparbat, gurbpa;
-		var dniv, dpor, cconlc_lib, dvlrt, jdatat, dnupro, dnomlp, dprnlp, expnee, dnomcp, dprncp
-		
-		//variable userrole: niveau de droits de l'utilisateur
-		var userrole;
-		
+        //déclaration des variables relatives aux données cadastrales
+        var ccodep, ccodir, ccocom, gcopre, ccosec, dnupla, dnupro, dnomlp, dprnlp, expnee, dnomcp, dprncp, adressehabitation , jdatnss , dldnss, ccodro_lib, dnvoiri, dindic, natvoiriv_lib, dvoilib, dcntpa, supf, gparbat, gurbpa;
+        var dniv, dpor, cconlc_lib, dvlrt, jdatat, dnupro, dnomlp, dprnlp, expnee, dnomcp, dprncp
+        
+        //variable userrole: niveau de droits de l'utilisateur
+        var userrole;
+        
 
     var FiucParcelleData = [ 
-								[ 'Commune','350250' ], 
-								[ 'Section','067AP' ] 
-							];
+                                [ 'Commune','350250' ], 
+                                [ 'Section','067AP' ] 
+                            ];
     var FiucProrietaireData =  [ 'col1','P' , '067AP' ,'067AP' ,'067AP' ,'067AP' ,'067AP' ,'067AP' ,'067AP','067AP','067AP' ];
-							
+                            
     var FiucBatimentsData = [ 
-   								[ 'Batiment 1' ], 
+                                   [ 'Batiment 1' ], 
                                 [ 'Batiment 2' ]
                                ];
-    var FiucSubdivfiscData = 			[[ 'col1','P' ], 
-										 ['067AP' ,'067AP'],
-										 ['zret','azr']];
+    var FiucSubdivfiscData =             [[ 'col1','P' ], 
+                                         ['067AP' ,'067AP'],
+                                         ['zret','azr']];
     var FiucHistomutData = [[ 'col1','P' ], 
-										 ['067AP' ,'067AP'],
-										 ['zret','azr']];
+                            ['067AP' ,'067AP'],
+                            ['zret','azr']];
 
 
     var FiucParcelleStore = new Ext.data.ArrayStore({
-		fields : [ {
+        fields : [ {
             name : 'designation'
         }, {
             name : 'valeur'
@@ -74,14 +74,14 @@ Ext.namespace("GEOR")
         } ],
         data : FiucProrietaireData
     });
-  	
+      
     var FiucBatimentsStore = new Ext.data.ArrayStore({
         fields : [ {
             name : 'batiments'
         }],
         data : FiucBatimentsData
     });
-  	
+      
     var FiucSubdivfiscStore = new Ext.data.ArrayStore({
         fields : [ {
             name : 'lettreindic'
@@ -94,7 +94,7 @@ Ext.namespace("GEOR")
         }],
         data : FiucSubdivfiscData
     });
-  	
+      
     var FiucHistomutStore = new Ext.data.ArrayStore({
         fields : [ {
             name : 'dateacte'
@@ -106,101 +106,101 @@ Ext.namespace("GEOR")
         data : FiucHistomutData
     });
 
-	//var sm = Ext.create('Ext.selection.CheckboxModel');
-  		
+    //var sm = Ext.create('Ext.selection.CheckboxModel');
+          
     FiucParcelleGrid = new Ext.grid.GridPanel({
         store : FiucParcelleStore,
         stateful : true,  
-		height : 500,
-		title: 'Bordereau parcellaire',
+        height : 500,
+        title: 'Bordereau parcellaire',
         name : 'Fiuc_Parcelle',
         xtype : 'editorgrid',
         colModel : new Ext.grid.ColumnModel({
             defaults : {
-				border : true,
+                border : true,
                 sortable : false,
             },
             columns : [ {
-            	header: OpenLayers.i18n('cadastrapp.duc.designation'),
+                header: OpenLayers.i18n('cadastrapp.duc.designation'),
                 dataIndex : 'designation'
-				}, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.valeur'),
+                }, 
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.valeur'),
                 dataIndex : 'valeur'
             }]
         }),
 
     });
-	
+    
     FiucProprietairesGrid = new Ext.grid.GridPanel({
         store : FiucProprietaireStore,
         stateful : true,
-		height : 500,
-		title: 'Relevé de propriété',
+        height : 500,
+        title: 'Relevé de propriété',
         name : 'Fiuc_Proprietaire',
         xtype : 'editorgrid',
         colModel : new Ext.grid.ColumnModel({
             defaults : {
-				border : true,
+                border : true,
                 sortable : false,
             },
-			
+            
             columns : [ 
-				{
+                {
 
-            	header: '',
-				//selModel: sm,				
-				 width: 50,
+                header: '',
+                //selModel: sm,                
+                 width: 50,
                 dataIndex : 'col1'
             }, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.compte'),
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.compte'),
                 dataIndex : 'compte'
             }, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.nom'),
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.nom'),
                 dataIndex : 'nom'
             }, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.prenom'),
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.prenom'),
                 dataIndex : 'prenom'
             }, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.mentioncpl'),
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.mentioncpl'),
                 dataIndex : 'mentioncpl'
             }, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.nomcpl'),
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.nomcpl'),
                 dataIndex : 'nomcpl'
             }, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.prenomcpl'),
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.prenomcpl'),
                 dataIndex : 'prenomcpl'
             }, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.adresse'),
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.adresse'),
                 dataIndex : 'adresse'
             }, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.datenaissance'),
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.datenaissance'),
                 dataIndex : 'datenaissance'
             }, {
-            	header: OpenLayers.i18n('cadastrapp.duc.lieunaissance'),
+                header: OpenLayers.i18n('cadastrapp.duc.lieunaissance'),
                 dataIndex : 'lieunaissance'
             },
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.cco_lib'),
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.cco_lib'),
                 dataIndex : 'cco_lib'
             }]
         }),
 
     });
-	
+    
     FiucBatimentsGrid = new Ext.grid.GridPanel({
         store : FiucBatimentsStore,
         stateful : true,
-		height : 500,
-		title: 'batiment(s)',
+        height : 500,
+        title: 'batiment(s)',
         name : 'Fiuc_Batiments',
         xtype : 'editorgrid',
         colModel : new Ext.grid.ColumnModel({
@@ -208,17 +208,17 @@ Ext.namespace("GEOR")
                 sortable : false,
             },
             columns : [ {
-            	header: OpenLayers.i18n('cadastrapp.CoProprietaire'),
+                header: OpenLayers.i18n('cadastrapp.CoProprietaire'),
                 dataIndex : 'batiments'
             }]
         }),
 
     });
-	
+    
     FiucSubdivfiscGrid = new Ext.grid.GridPanel({
         store : FiucSubdivfiscStore,
         stateful : true,
-		height : 500,
+        height : 500,
         name : 'Fiuc_Subdivisions_fiscales',
         xtype : 'editorgrid',
         colModel : new Ext.grid.ColumnModel({
@@ -226,30 +226,30 @@ Ext.namespace("GEOR")
                 sortable : false,
             },
             columns : [ {
-            	header: OpenLayers.i18n('cadastrapp.duc.lettreindic'),
+                header: OpenLayers.i18n('cadastrapp.duc.lettreindic'),
                 dataIndex : 'lettreindic'
-			}, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.contenance'),
+            }, 
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.contenance'),
                 dataIndex : 'contenance'
             }, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.terrain'),
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.terrain'),
                 dataIndex : 'terrain'
             }, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.revenu'),
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.revenu'),
                 dataIndex : 'revenu'
             }]
         }),
 
     });
 
-	
+    
     FiucHistomutGrid = new Ext.grid.GridPanel({
         store : FiucHistomutStore,
         stateful : true,
-		height : 500,
+        height : 500,
         name : 'Fiuc_Historique_Mutation',
         xtype : 'editorgrid',
         colModel : new Ext.grid.ColumnModel({
@@ -257,95 +257,95 @@ Ext.namespace("GEOR")
                 sortable : false,
             },
             columns : [ {
-            	header: OpenLayers.i18n('cadastrapp.duc.dateacte'),
+                header: OpenLayers.i18n('cadastrapp.duc.dateacte'),
                 dataIndex : 'dateacte'
-			}, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.reference_parcelle'),
+            }, 
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.reference_parcelle'),
                 dataIndex : 'reference.parcelle'
             }, 
-				{
-            	header: OpenLayers.i18n('cadastrapp.duc.type_mutation'),
+                {
+                header: OpenLayers.i18n('cadastrapp.duc.type_mutation'),
                 dataIndex : 'type.mutation'
             }]
         }),
 
     });
-					
+                    
 
-  		//Construction de la fenêtre principale
-		var windowFIUC;
-			windowFIUC = new Ext.Window({
+          //Construction de la fenêtre principale
+        var windowFIUC;
+            windowFIUC = new Ext.Window({
             title: 'TODO',
-			frame: true,
-			autoScroll:true,
-			minimizable: false,
-			closable: true,
-			resizable: false,
-			draggable : true,
-			constrainHeader: true,
-			
-			border:false,
-			labelWidth: 400,
-			width: 850,		
-			defaults: {autoHeight: true, bodyStyle:'padding:10px', flex: 1},
+            frame: true,
+            autoScroll:true,
+            minimizable: false,
+            closable: true,
+            resizable: false,
+            draggable : true,
+            constrainHeader: true,
+            
+            border:false,
+            labelWidth: 400,
+            width: 850,        
+            defaults: {autoHeight: true, bodyStyle:'padding:10px', flex: 1},
 
-			listeners: {
-				close: function(window) {
-					windowFIUC = null;
-				}
-			},
-			
-			items: [{
-				autoHeight: true,	
-				xtype: 'tabpanel', width:800, height: 800,	
-				activeTab: 0,				
-					items: [{
-								//ONGLET 1
-								title: OpenLayers.i18n('cadastrapp.duc.parcelle'),	
-								xtype:'form',
-								items: [
-									FiucParcelleGrid
-										]
-									},
-									{
-								//ONGLET 2
-								title: OpenLayers.i18n('cadastrapp.duc.propietaire'),
-								xtype:'form',
-								items: [
-									FiucProprietairesGrid
-										]
-									},
-									{
-								//ONGLET 3
-								title: OpenLayers.i18n('cadastrapp.duc.batiment'),
-								xtype:'form',
-								items: [
-									FiucBatimentsGrid
-										]
-									},
-									{
-								//ONGLET 4
-								title: OpenLayers.i18n('cadastrapp.duc.subdiv'),
-								xtype:'form',
-								items: [
-									FiucSubdivfiscGrid
-										]
+            listeners: {
+                close: function(window) {
+                    windowFIUC = null;
+                }
+            },
+            
+            items: [{
+                autoHeight: true,    
+                xtype: 'tabpanel', width:800, height: 800,    
+                activeTab: 0,                
+                    items: [{
+                                //ONGLET 1
+                                title: OpenLayers.i18n('cadastrapp.duc.parcelle'),    
+                                xtype:'form',
+                                items: [
+                                    FiucParcelleGrid
+                                        ]
+                                    },
+                                    {
+                                //ONGLET 2
+                                title: OpenLayers.i18n('cadastrapp.duc.propietaire'),
+                                xtype:'form',
+                                items: [
+                                    FiucProprietairesGrid
+                                        ]
+                                    },
+                                    {
+                                //ONGLET 3
+                                title: OpenLayers.i18n('cadastrapp.duc.batiment'),
+                                xtype:'form',
+                                items: [
+                                    FiucBatimentsGrid
+                                        ]
+                                    },
+                                    {
+                                //ONGLET 4
+                                title: OpenLayers.i18n('cadastrapp.duc.subdiv'),
+                                xtype:'form',
+                                items: [
+                                    FiucSubdivfiscGrid
+                                        ]
 
-									},
-									{
-									//ONGLET 5
-									title:OpenLayers.i18n('cadastrapp.duc.histomut'),
-									xtype:'form',					
-									items: [
-											FiucHistomutGrid,	//grille "historique de mutation"
-											]
-									}]
-								}]
-			
-			});
-		windowFIUC.show();
+                                    },
+                                    {
+                                    //ONGLET 5
+                                    title:OpenLayers.i18n('cadastrapp.duc.histomut'),
+                                    xtype:'form',                    
+                                    items: [
+                                            FiucHistomutGrid,    //grille "historique de mutation"
+                                            ]
+                                    }]
+                                }]
+            
+            });
+        windowFIUC.show();
     console.log("displayFIUC onClick")
-	};
+    };
 	
 	
