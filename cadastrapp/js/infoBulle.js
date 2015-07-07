4d5a65ff72a804dedf4c089fceb3e66e7d6f817a
pierrejego <pierre.jego@gfi.fr>
2015-07-07 10:19:23 +0200

    Change infobulle display mode

--- cadastrapp/js/infoBulle.js
+++ cadastrapp/js/infoBulle.js
@@ -5,176 +5,44 @@
  */
 Ext.namespace("GEOR")
 
-//onClickDisplayInfoBulle = function(ccocom, ccodep, ccodir, ccopre, 
-//        ccosec, dindic, dnupla, dnvoiri, dvoilib, parcelleId ){
-    
-onClickDisplayInfoBulle = function() {
+/**
+ *  Information to be display in infobulle
+ *  
+ * @param parcelleId parameter will be use to call webapp and get additional information
+ */    
+onClickDisplayInfoBulle = function(parcelleId) {
 
-    // parcelle utilise pour la requete demo
-    var parcelle = '2014630103000YA0054';
-
-    var parcelleId = "025";
-    var libcom = "CLERMONT";
-    var ccocom = "63005";
-    var ccodep = "63";
-    var ccodir = "0";
-    var ccopre = "";
-    var ccosec = "AZ";
-    var cconvo = "rue";
-    var dindic = "";
-    var dnupla = "0958";
-    var dnvoiri = "16";
-    var dcntpa_sum = "789.56";
-    var sigcal_sum = "712.22";
-    var batical = "156.5";
-    var dvoilib = "PUY DES GOUTTES";
-    var comptecommunal = "XXXX";
-    var dnupro="dupond";
-    var dcntpa;
-    // INIT infobulle
-
-    // requete vers la WEBAPP, se referer aux specs
+    // webapp request using parcelleid
     Ext.Ajax.request({
         url : getWebappURL() + 'getInfoBulle?parcelle=' + parcelle,
         failure : function() {
-            alert("erreur lors de la requete 'getInfoBulle' ");
+        	//TODO change i18n
+            alert("Erreur lors de la requete 'getInfoBulle' ");
         },
         method : "GET",
         success : function(response, opts) {
             var obj = Ext.decode(response.responseText);
+            
+            //TODO set console only to debug
             console.log(obj);
+            
             libcom = obj[0].libcom;
             dcntpa = obj[0].dcntpa;
-			//todo: dcntpa_sum = obj[0].dcntpa_sum;
+			//TODO add when available in webapp dcntpa_sum = obj[0].dcntpa_sum;
             dnvoiri = obj[0].dnvoiri;
             dindic = obj[0].dindic;
             dvoilib = obj[0].dvoilib;
             dnupro = obj[0].dnupro;
             dnomlp = obj[0].dnomlp;
             comptecommunal = obj[0].comptecommunal;
-            dcntpa_sum = obj[0].dcntpa_sum;
-            sigcal_sum = obj[0].sigcal_sum;
-            batical = obj[0].batical;
-            //TODO : tout les infos a afficher sont a passes en param
-            
+            //TODO add when available in webapp dcntpa_sum = obj[0].dcntpa_sum;
+            //TODO add when available in webapp sigcal_sum = obj[0].sigcal_sum;
+            //TODO add when available in webapp batical = obj[0].batical;
+           
+            //TODO check how to display information in GeoExt probably return html
         }
 
-    });
-	if ((isCadastre()===true) && (isFoncier()===false)) 
-			
-		{
-			alert('iscadastre=true and isfoncier=false'),
-			//TODO : a remettre ans le success de l'ajax une fois le Webservice OK
-			displayInfoBulle(comptecommunal);
-		} 
-	if ((isCadastre()===false) && (isFoncier()===true)) 
-		
-		{
-			alert('iscadastre=false and isfoncier=true'),
-			//TODO : a remettre ans le success de l'ajax une fois le Webservice OK
-			displayInfoBulle(comptecommunal);
-		} 
-		
-	if ((isCadastre()===true) && (isFoncier()===true)) 
-		
-		{
-			alert('iscadastre=true and isfoncier=true'),
-			//TODO : a remettre ans le success de l'ajax une fois le Webservice OK
-			displayInfoBulle(comptecommunal);
-		} 
-	if ((isCadastre()===false) && (isFoncier()===false)) 
-		
-		
-			alert('iscadastre=false and isfoncier=false')
-			//TODO : a remettre ans le success de l'ajax une fois le Webservice OK
-		//displayInfoBulle(comptecommunal);
-		
+    });	
 				
-				
-};
-
-function displayInfoBulle(comptecommunal) {
-
-    // Construction de la fenêtre principale
-    var windowDisplayInfoBulle;
-    windowDisplayInfoBulle = new Ext.Window({
-
-        frame : true,
-        autoScroll : true,
-        minimizable : false,
-        closable : true,
-        resizable : false,
-        draggable : true,
-        constrainHeader : true,
-
-        border : false,
-        // labelWidth : 100,
-        // width : 300,
-        defaults : {
-            autoHeight : true,
-            autoWidth : true,
-            bodyStyle : 'padding:1px',
-            flex : 1
-        },
-
-        /*
-         * listeners: { close: function(window) { windowDisplayInfoBulle = null; },
-         */
-        items : [ {
-            xtype : 'fieldset',
-            font : '6px Arial',
-            labelWidth : 0,
-            padding : 5,
-            autoHeight : true,
-            defaultType : 'label',
-            items : [
-
-//            {
-//                fieldLabel : ' ',
-//                xtype : 'label',
-//                text : libcom + '(' + ccocom + ')'
-//            }, {
-//                xtype : 'label',
-//                text : parcelleId + ccosec
-//            }, {
-//                xtype : 'label',
-//                html : "<br />"
-//            }, {
-//                xtype : 'label',
-//                text : dnvoiri + ' ' + dindic + ' ' + cconvo + ' ' + dvoilib
-//            }, {
-//                fieldLabel : ' ',
-//                xtype : 'label',
-//                text : dcntpa_sum + 'm2'
-//            }, {
-//                fieldLabel : ' ',
-//                xtype : 'label',
-//                text : sigcal_sum + 'm2'
-//            }, 
-            {
-                xtype : 'label',
-                value : comptecommunal,
-                hideLabel : true
-            }, 
-//            {
-//                fieldLabel : ' ',
-//                xtype : 'label',
-//                text : dcntpa_sum
-//            }, {
-//                fieldLabel : ' ',
-//                xtype : 'label',
-//                text : sigcal_sum
-//            }, {
-//                fieldLabel : ' ',
-//                xtype : 'label',
-//                text : surfbati
-//            } 
-            ]
-
-        } ],
-
-//		 }
-    });
-
-    windowDisplayInfoBulle.show();
 }
+
