f338dac84a8f82f5450471e035052c4e141e56e8
laurentcornic <laurent.cornic@gfi.fr>
2015-07-09 10:39:10 +0200

    Modifications des stores et ajout des appels ajax pour recuperer les donnees des unites foncieres

--- cadastrapp/js/detailUniteFonciere.js
+++ cadastrapp/js/detailUniteFonciere.js
@@ -6,66 +6,96 @@
 Ext.namespace("GEOR")
 
 /**
- * public: method[onClickdisplayFIUF] :param layer: Create ...TODO
+ * onClickdisplayFIUF
+ * 
+ * @param layer: parcelleId
+ * Cette methode construit la fiche d'information foncière pour une parcelle donnée
+ * dont d'identifiant est parcelleId
+ * Elle permet également l'export au format pdf de la fiche d'information foncière
+ * @return Test
  */
 onClickDisplayFIUF = function(parcelleId) {
     var windowFIUF, parcelleGrid;
 
  //   var FiufGlobalInfosData = [ [ 'Surface DGFIP', "1420" ],
  //           [ 'Surface SIG', "1423" ], [ 'Surface batie', "200" ] ];
- 
+    
+    // Declaration des donnees globales
     var FiufGlobalInfosData = 	[];
 
+    // Declaration des donnees propprietaires
     var FiufProprietaireData = [ [ 'Proprietaire 1' ], [ 'Proprietaire 2' ] ];
+    
+    // Declaration des donnees de liste de parcelles
     var FiufParcelleListData = [ [ '38852 2225 22', '255','201',"1 Rue louis 1"], 
                                  [ '38852 2225 22', '255','201',"1 Rue louis 1"],
                                  [ '38852 2225 22', '255','201',"1 Rue louis 1"] ];
-								 
+	
+    // Declaration des variables 							 
 	var surfaceDGFiP ='';
 	var surfaceSIG ='';
 	var surfaceBatie ='';
 
+	// Déclaration du bouton d'export PDF de la fiche d'unité foncière
+	var exportPdfFiufButton = new ext.ButtonGroup({
+		bodyBorder: false,
+		border: false,
+		hideBorders: false,
+		frame: false,
+		items:[{
+			xtype: 'button',
+			scale: 'small',
+			name: 'Export pdf',
+			iconCls: "pdf-button",
+			handler: function() {
+				// TODO: call createPdfUF
+			}						
+		}]	
+		
+	});
 	
+	// Modele de donnees informations globales de la parcelle
 	var FiufGlobalInfosStore =new Ext.data.ArrayStore({
-			fields : [ {
-				name : 'uniteFonciere'
-			}, {
-				name : 'surface',
-				type : 'float'
-			}, ],
-			data : FiufGlobalInfosData
-         });
-					
-		 Ext.Ajax.request({
+	fields : [ {
+		name : 'uniteFonciere'
+		}, {
+		name : 'surface',
+		type : 'float'
+		}, ],
+	data : FiufGlobalInfosData
+	});
+		
+	//Requete Ajax pour chercher dans la webapp les donnees relative à la parcelle
+	// TODO requete getFIUF a completer
+	Ext.Ajax.request({
         url: getWebappURL() + 'getFIUF?parcelle='+parcelleId+"&detail=1",
         method: 'GET',   
-        //params: params,
+        //Evaluation de la reponse et memorisation des champs
+        //params: response,
         success: function(response) {
-            //console.log(response.responseText);
+        	//console.log(response.responseText);
             var result = eval(response.responseText);
-            surfaceDGFiP = result[0].dcntpa_sum;
-			
-            surfaceSIG = result[0].sigcal_sum;
-			
+            
+            // Les champs retournés par la webapp sont mis dans 
+            // les variables correspondantes
+            surfaceDGFiP = result[0].dcntpa_sum;			
+            surfaceSIG = result[0].sigcal_sum;			
             surfaceBatie = result[0].batical_sum;
-			
- 
-            //console.log(commune);
            
+            //Preparation du tableau de donnees
             FiufGlobalInfosData = [[ 'Unite Fonciere', surfaceDGFiP ],
                                  [ ' Surface SIG', surfaceSIG ],
                                  [ 'Surface Batie', surfaceBatie ],
 								];
+            //Chargement dans le store correspondant
             FiufGlobalInfosStore.loadData(FiufGlobalInfosData,false);
-			
-            data : FiufGlobalInfosData;
-			 
-			//titre de la fenetre
-			//titleFIUC =result[0].ccodep + result[0].ccodir + result[0].ccocom + '-'+result[0].ccopre + result[0].ccosec+'-'+result[0].dnupla;
-            console.log(titleFIUC);         
+			data : FiufGlobalInfosData;
+			        
         }
     }); 
-
+	
+	// Modele de donnes relatif à la liste des coproprietaires
+	// TODO a modifier
     var FiufProprietaireStore = new Ext.data.ArrayStore({
         fields : [ {
             name : 'proprietaire'
@@ -73,6 +103,9 @@ onClickDisplayFIUF = function(parcelleId) {
         data : FiufProprietaireData
     });
     
+/*  // 
+ * 	// TODO	
+ * Modele de donnes relatif aux listes de parcelles
     var FiufParcelleListStore = new Ext.data.ArrayStore({
         fields : [ {
             name : 'parcelle',
@@ -92,8 +125,24 @@ onClickDisplayFIUF = function(parcelleId) {
         },],
         
         data : FiufParcelleListData
+    });*/
+    // Modele de donnes relatif aux listes de parcelles
+    var FiufParcelleListStore = new Ext.data.JsonStore({
+    	
+    	// Appel a la webapp
+    	url: getWebappURL()+'getFIUF?parcelle='+parcelleId+"&detail=1",
+    	autoLoad : true,
+    	fields : [
+            'comptecommunal',
+            'dcntpa_sum',
+            'sigcal_sum',
+            'adressepostale'
+            ],
+        
+        data : FiufParcelleListData
     });
-
+    // Declaration et construction du tableau informations globales de la parcelle
+    // Tableau composé des colonnes 'unité foncière' et 'surface'
     FiufGlobalInfosGrid = new Ext.grid.GridPanel({
         store : FiufGlobalInfosStore,
         stateful : true,
@@ -119,6 +168,8 @@ onClickDisplayFIUF = function(parcelleId) {
         border : true,
     });
 
+    // Declaration et construction du tableau des coproprietaires
+    // Composé d'une seule colonne: co-proprietaires
     FiufProprietaireGrid = new Ext.grid.GridPanel({
         store : FiufProprietaireStore,
         stateful : true,
@@ -140,6 +191,8 @@ onClickDisplayFIUF = function(parcelleId) {
         border : true,
     });
 
+    // Declaration et construction du tableau de liste de parcelles
+    // Comprend les colonnes 'parcelle', 'surface DGFiP','Surface SIG' et 'Adresse postale'
     FiufParcelleListGrid = new Ext.grid.GridPanel({
         store : FiufParcelleListStore,
         stateful : true,
@@ -152,25 +205,29 @@ onClickDisplayFIUF = function(parcelleId) {
             },
             columns : [
                     {
+                    	// colonne parcelle
                         header : OpenLayers.i18n('cadastrapp.parcelle'),
                         width : 100,
-                        dataIndex : 'parcelle'
+                        dataIndex : 'comptecommunal'
                     },
                     {
+                    	// colonne surface DGFiP
                         header : OpenLayers.i18n('cadastrapp.surface') + " "
-                                + OpenLayers.i18n('cadastrapp.parcelle.DGFIP'),
+                                + OpenLayers.i18n('cadastrapp.parcelle.DGFiP'),
                         width : 50,
-                        dataIndex : 'surfacedgfip'
+                        dataIndex : 'dcntpa_sum'
                     },
                     {
+                    	// colonne surface SIG
                         header : OpenLayers.i18n('cadastrapp.surface') + " "
                                 + OpenLayers.i18n('cadastrapp.parcelle.SIG'),
                         width : 50,
-                        dataIndex : 'surfacesig'
+                        dataIndex : 'sigcal_sum'
                     }, {
+                    	//colonne adresse postale
                         header : OpenLayers.i18n('cadastrapp.adresse_postale'),
                         width : 200,
-                        dataIndex : 'adresse'
+                        dataIndex : 'adressepostale'
                     } ],
         }),
         height : 100,
@@ -178,11 +235,8 @@ onClickDisplayFIUF = function(parcelleId) {
         border : true,
     });
 
-    FiufDownloadPdfButton = new Ext.Button({
-        name : 'FiufDownloadPdfButton',
-        cls : "pdf_button"
-        
-    });
+    // Declaration et creation de la fenetre principale
+    // Comprend les tableaux 'unite fonciere', 'coproprietaires' et 'liste des parcelles
     windowFIUF = new Ext.Window({
         title : parcelleId,
         frame : true,
@@ -201,11 +255,11 @@ onClickDisplayFIUF = function(parcelleId) {
                 left : 10
             },
 
-            items : [ 
+            items : [[ 
                       FiufGlobalInfosGrid, 
                       FiufProprietaireGrid,
-                      FiufDownloadPdfButton
-                    ]
+                      
+                    ],[exportPdfFiufButton]]
         }, 
             FiufParcelleListGrid 
         ],
