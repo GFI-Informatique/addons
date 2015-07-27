c357a3ff3e8309b7d98eee26ab41e324a4a03f05
pierrejego <pierre.jego@gfi.fr>
2015-07-27 14:58:45 +0200

    Debug why Cnil level is not updated when changing user.

--- cadastrapp/js/cadastrappUtil.js
+++ cadastrapp/js/cadastrappUtil.js
@@ -5,6 +5,7 @@ Ext.namespace("GEOR.Addons.Cadastre");
  * @returns {Boolean}
  */
 GEOR.Addons.Cadastre.isCNIL1 = function() {
+    console.log("Test cnil level1");
     return (GEOR.config.ROLES.indexOf(GEOR.Addons.Cadastre.cnil1RoleName) != -1);
 };
   
@@ -13,6 +14,7 @@ GEOR.Addons.Cadastre.isCNIL1 = function() {
  * @returns {Boolean}
  */
 GEOR.Addons.Cadastre.isCNIL2 = function() {
+    console.log("Test cnil level2");
     return (GEOR.config.ROLES.indexOf(GEOR.Addons.Cadastre.cnil2RoleName) != -1);
 };
 
