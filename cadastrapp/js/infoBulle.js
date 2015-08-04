b439d98bc9a673008923a1be0e52b97e008b4f0b
pierrejego <pierre.jego@gfi.fr>
2015-08-04 16:20:30 +0200

    Replace for loop in Ext.each

--- cadastrapp/js/infoBulle.js
+++ cadastrapp/js/infoBulle.js
@@ -14,85 +14,85 @@ Ext.namespace("GEOR.Addons.Cadastre");
  */    
 GEOR.Addons.Cadastre.displayInfoBulle = function(map, idParcelle, lonlat) {
 
-	// Build url depending on check button Cadastre or Foncier
-	urlInfoBulleService =  GEOR.Addons.Cadastre.cadastrappWebappUrl+ 'getInfoBulle?parcelle=' + idParcelle
-	
-	if (!GEOR.Addons.Cadastre.isCadastre()){
-		urlInfoBulleService += "&infocadastrale=0";
-	}
-	if (!GEOR.Addons.Cadastre.isFoncier()){
-		urlInfoBulleService += "&infouf=0";
-	}
+    // Build url depending on check button Cadastre or Foncier
+    urlInfoBulleService =  GEOR.Addons.Cadastre.cadastrappWebappUrl+ 'getInfoBulle?parcelle=' + idParcelle
+    
+    if (!GEOR.Addons.Cadastre.isCadastre()){
+        urlInfoBulleService += "&infocadastrale=0";
+    }
+    if (!GEOR.Addons.Cadastre.isFoncier()){
+        urlInfoBulleService += "&infouf=0";
+    }
     // webapp request using parcelleid
     Ext.Ajax.request({
         url : urlInfoBulleService,
         failure : function() {
-        	//TODO change i18n
+            //TODO change i18n
             alert("Erreur lors de la requete 'getInfoBulle' ");
         },
         method : "GET",
         success : function(response, opts) {
           
-        	// result from requestion JSON
-        	var result = Ext.decode(response.responseText);
-       	   	
-        	html = "";
-        	if (typeof(result) != "undefined"){
-        	
-            	if (GEOR.Addons.Cadastre.isCadastre()){
-            		html = "<div class=\"cadastrapp-infobulle-parcelle\"><div>"+result.libcom+"</div>" +
-            		"<div>"+idParcelle+"</div>" +
-            		"<div>"+result.dnvoiri+" "+result.dindic+" "+result.cconvo+" "+result.dvoilib+"</div>" +
-            		"<div>"+OpenLayers.i18n('cadastrapp.contenancedgfip') +" : "+result.dcntpa.toLocaleString()+" m²</div>" +
-        			"<div>"+OpenLayers.i18n('cadastrapp.sig') +" : "+result.surfc.toLocaleString()+" m²</div>";
-            		
-            		if(GEOR.Addons.Cadastre.isCNIL1() || GEOR.Addons.Cadastre.isCNIL2()){
-            			if (typeof(result.proprietaires) != "undefined"){
-                    		for(i=0;i<result.proprietaires.length; i++){
-                    		    if(i=4){
-                    		        html = html + "<div>...  </div>";
-                    		    }else{
-                    		        html = html + "<div>"+ result.proprietaires[i].ddenom +"</div>";
-                    		    }    			    
-                    		}
-                    	}
-            		}
-            		html += "</div>"
-            	}
-            	if (GEOR.Addons.Cadastre.isFoncier()){
-            		//TODO wait for data from view
-            		html += "<div class=\"cadastrapp-infobulle-unite-fonciere\">" +
-            				"<div>"+result.comptecommunal +"</div>" +
-            		        "<div>"+ OpenLayers.i18n('cadastrapp.contenancedgfip') +" UF : "+result.dcntpa_sum.toLocaleString()+" m²</div>" +
-            		        "<div>"+ OpenLayers.i18n('cadastrapp.sig') +" UF :"+result.sigcal_sum.toLocaleString()+" m²</div>";
+            // result from requestion JSON
+            var result = Ext.decode(response.responseText);
+            
+            html = "";
+            if (typeof(result) != "undefined"){
+            
+                if (GEOR.Addons.Cadastre.isCadastre()){
+                    html = "<div class=\"cadastrapp-infobulle-parcelle\"><div>"+result.libcom+"</div>" +
+                    "<div>"+idParcelle+"</div>" +
+                    "<div>"+result.dnvoiri+" "+result.dindic+" "+result.cconvo+" "+result.dvoilib+"</div>" +
+                    "<div>"+OpenLayers.i18n('cadastrapp.contenancedgfip') +" : "+result.dcntpa.toLocaleString()+" m²</div>" +
+                    "<div>"+OpenLayers.i18n('cadastrapp.sig') +" : "+result.surfc.toLocaleString()+" m²</div>";
+                    
+                    if(GEOR.Addons.Cadastre.isCNIL1() || GEOR.Addons.Cadastre.isCNIL2()){
+                        if (typeof(result.proprietaires) != "undefined"){
+                            Ext.each(result.proprietaires, function(proprietaire, currentIndex){
+                                if(currentIndex==4){
+                                    html = html + "<div>...  </div>";
+                                }else{
+                                    html = html + "<div>"+ result.proprietaires[i].ddenom +"</div>";
+                                }                   
+                            });
+                        }
+                    }
+                    html += "</div>"
+                }
+                if (GEOR.Addons.Cadastre.isFoncier()){
+                    //TODO wait for data from view
+                    html += "<div class=\"cadastrapp-infobulle-unite-fonciere\">" +
+                            "<div>"+result.comptecommunal +"</div>" +
+                            "<div>"+ OpenLayers.i18n('cadastrapp.contenancedgfip') +" UF : "+result.dcntpa_sum.toLocaleString()+" m²</div>" +
+                            "<div>"+ OpenLayers.i18n('cadastrapp.sig') +" UF :"+result.sigcal_sum.toLocaleString()+" m²</div>";
                     //TODO add when available in webapp batical = result[0].batical;
-            		html += "</div>";
-            	}
-  	
+                    html += "</div>";
+                }
+    
                 popup = new GeoExt.Popup({
-    				map:map,
-    				location: lonlat,
-    				width: 200,
-    				html: html,
-    				listeners: {
+                    map:map,
+                    location: lonlat,
+                    width: 200,
+                    html: html,
+                    listeners: {
                         close: function() {
                             // closing a popup destroys it, but our reference is truthy
                             popup = null;
                         }
                     }
-    			});
+                });
                 
-    			popup.show();
-			
-    			// TODO check to avoid erase too quickly
-    			document.body.onmousemove = function(e) {
-    			    //destroy popup on move
-    			    popup.destroy()
-    			}
-        	}
-       	}
-    });	
+                popup.show();
+            
+                // TODO check to avoid erase too quickly
+                document.body.onmousemove = function(e) {
+                    //destroy popup on move
+                    popup.destroy()
+                }
+            }
+        }
+    }); 
     
-				
+                
 }
 
